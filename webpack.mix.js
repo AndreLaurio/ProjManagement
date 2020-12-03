f0e76e24f1fafa6ecfe4803c145ad44f9d8352cb
Andre Laurio <45072321+Hyperstorm321@users.noreply.github.com>
2020-12-03 15:27:50 +0800

    Revert "tailwindcss added"
    This reverts commit f798e7845aef9cc2bd4b94a254a0a4a31410eebf.

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,7 +24,5 @@ mix.webpackConfig( webpackConfig );
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-.postCss("resources/css/app.css", "public/css", [
- require("tailwindcss"),
-]);
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
